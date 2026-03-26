be625af1a35ba9b03c4ce75aab5628c207f774ee
shamb1es <juwright24@gmail.com>
2026-03-26 12:55:50 -0400

    Cleaned analysis file + Initial Power BI report

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -18,10 +18,14 @@ DELIMITER ;
 
 # View that calculates distances between all sensors in each data set
 CREATE OR REPLACE VIEW sensor_distances AS
-SELECT p.sensor_index, a.site_id,
-ROUND(haversine_miles(p.latitude, p.longitude, a.latitude, a.longitude),2) AS dist_miles
-FROM purpleair_sensors p
-CROSS JOIN airnow_sites a;
+SELECT sensor_index, site_id, MIN(dist_miles) FROM
+	(SELECT p.sensor_index, a.site_id,
+	ROUND(haversine_miles(p.latitude, p.longitude, a.latitude, a.longitude),2) AS dist_miles
+	FROM purpleair_sensors p
+	CROSS JOIN airnow_sites a) o
+;
+
+select * from sensor_distances limit 100;
 
 # Procedure to group each PM2.5 values in their corresponding AQI group
 DROP PROCEDURE IF EXISTS pm25_averages;
@@ -65,16 +69,20 @@ CALL pm25_averages('pm25_raw_concentration', 'airnow_sensor_data');
 
 DROP PROCEDURE IF EXISTS correlation;
 DELIMITER //
-CREATE PROCEDURE correlation (IN x VARCHAR(11), IN y VARCHAR(11), 
+CREATE PROCEDURE correlation (IN x VARCHAR(64), IN y VARCHAR(64), 
 IN which_table VARCHAR(64))
 BEGIN
 	SET @corrquer = CONCAT(
-        'SELECT ', x, ', ', y, ',
+        'SELECT 
         (AVG(', x, ' * ', y, ') - 
          AVG(', x, ') * AVG(', y, ')) /
         (
-         SQRT(AVG(', x, ' * ', x, ') - POW(AVG(', x, '), 2)) *
-         SQRT(AVG(', y, ' * ', y, ') - POW(AVG(', y, '), 2))
+         NULLIF(
+			(
+				SQRT(AVG(',x,' * ',x,') - POW(AVG(',x,'), 2)) *
+				SQRT(AVG(',y,' * ',y,') - POW(AVG(',y,'), 2))
+			), 0
+		)
         ) AS correlation_coefficient
         FROM ', which_table
     );
@@ -86,59 +94,21 @@ END //
 DELIMITER ;
 
 CALL correlation('pm25_atm_a', 'pm25_atm_b', 'purpleair_sensor_data');
+CALL correlation('pm25_cf_1_a', 'pm25_cf_1_b', 'purpleair_sensor_data');
 
-select * from purpleair_sensor_data limit 10;
+select * from purpleair_sensor_data limit 100;
 
-
-
-
-
-
-select * from airnow_sites;
-
-select * from airnow_sensor_data limit 50;
-
-
-
-
-# Check where PM2.5 values are 0
-SELECT SUM(`pm2.5_atm_a` = 0) AS '0_pm2.5_atm_a', SUM(`pm2.5_atm_b` = 0) AS '0_pm2.5_atm_b',
-SUM(`pm2.5_cf_1_a` = 0) AS '0_pm2.5_cf_1_a', SUM(`pm2.5_cf_1_b` = 0) AS '0_pm2.5_cf_1_b'
-FROM staging_purpleair_sensor_data;
-
-# Check where PM2.5 values are ALL 0 (possibly a reboot value, faulty, etc.)
-SELECT sensor_index,
-SUM(`pm2.5_atm_a` = 0 AND `pm2.5_atm_b` = 0 AND `pm2.5_cf_1_a` = 0 AND `pm2.5_cf_1_b` = 0) AS all_empty,
-COUNT(*) AS total
-FROM staging_purpleair_sensor_data
-GROUP BY sensor_index
-HAVING all_empty > 0
-ORDER BY all_empty DESC;
-
-# All sensor PM2.5 values at least 0
 SELECT *
-FROM staging_purpleair_sensor_data
-WHERE `pm2.5_atm_a` < 0 OR `pm2.5_atm_b` < 0 OR `pm2.5_cf_1_a` < 0 OR `pm2.5_cf_1_b` < 0;
+FROM purpleair_sensor_data
+WHERE (`pm25_atm_a` > 300) OR (`pm25_atm_b` > 300) OR (`pm25_cf_1_a` > 300) OR (`pm25_cf_1_b` > 300)
+ORDER BY sensor_index, datetime_timestamp;
 
-# Verified greatest PM2.5 value in the AirNow dataset is
-SELECT COUNT(*)
-FROM staging_purpleair_sensor_data
-WHERE `pm2.5_atm_a` > 300  OR `pm2.5_atm_b` > 300 OR `pm2.5_cf_1_a` > 300 OR `pm2.5_cf_1_b` > 300;
+SELECT AVG(pm25_atm_a) FROM purpleair_sensor_data WHERE pm25_atm_a > 300
+UNION ALL
+SELECT AVG(pm25_atm_b) FROM purpleair_sensor_data WHERE pm25_atm_b > 300
+UNION ALL
+SELECT AVG(pm25_cf_1_a) FROM purpleair_sensor_data WHERE pm25_cf_1_a > 300
+UNION ALL
+SELECT AVG(pm25_cf_1_b) FROM purpleair_sensor_data WHERE pm25_cf_1_b > 300;
 
-select avg(`pm2.5_atm_a`), avg(`pm2.5_atm_b`), avg(`pm2.5_cf_1_a`), avg(`pm2.5_cf_1_b`)
-from staging_purpleair_sensor_data
-WHERE `pm2.5_atm_a` < 300  AND `pm2.5_atm_b` < 300 AND `pm2.5_cf_1_a` < 300 AND `pm2.5_cf_1_b` < 300;
-
-select *
-from staging_purpleair_sensor_data
-WHERE (`pm2.5_atm_a` < 150 AND `pm2.5_atm_a` > 100)
-  OR (`pm2.5_atm_b` < 150 AND `pm2.5_atm_b` > 100)
-  OR (`pm2.5_cf_1_a` < 150 AND `pm2.5_cf_1_a` > 100) 
-  OR (`pm2.5_cf_1_b` < 150 AND `pm2.5_cf_1_b` > 100)
-ORDER BY sensor_index, time_stamp;
-
-# Only contains 126 more rows than previous query (64658 above and 64784 here)
-SELECT *
-FROM staging_purpleair_sensor_data
-WHERE ((`pm2.5_atm_a` < 300 AND `pm2.5_atm_b` > 300) OR (`pm2.5_atm_b` < 300 AND `pm2.5_atm_a` > 300))
-OR ((`pm2.5_cf_1_a` < 300 AND `pm2.5_cf_1_b` > 300) OR (`pm2.5_cf_1_b` < 300 AND `pm2.5_cf_1_a` > 300));
+SELECT * FROM purpleair_sensor_data limit 10;
