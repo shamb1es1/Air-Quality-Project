40040acb68b5bdc8bee1c6956c563b1404043754
shamb1es <juwright24@gmail.com>
2026-04-22 16:02:39 -0400

    Expanded procedured + fixed correlation

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -41,7 +41,7 @@ CALL pm25_averages('pm25_raw_concentration', 'airnow_sensor_data');
 DROP PROCEDURE IF EXISTS correlation;
 DELIMITER //
 CREATE PROCEDURE correlation (IN x VARCHAR(64), IN y VARCHAR(64), 
-IN which_table VARCHAR(64))
+IN which_table VARCHAR(64), IN where_clause VARCHAR(999))
 BEGIN
 	SET @corrquer = CONCAT(
         'SELECT 
@@ -55,7 +55,7 @@ BEGIN
 			), 0
 		)
         ) AS correlation_coefficient
-        FROM ', which_table
+        FROM ', which_table, ' ', where_clause
     );
 
     PREPARE getcorr FROM @corrquer;
@@ -64,8 +64,31 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL correlation('pm25_atm_a', 'pm25_atm_b', 'purpleair_sensor_data');
-CALL correlation('pm25_cf_1_a', 'pm25_cf_1_b', 'purpleair_sensor_data');
+SELECT * FROM purpleair_sensor_data;
+
+# Near perfect correlation for a->a and b->b conversions which is expected as a formula conversion,
+# but both a/b comparisons have extremely small (~-0.0157) coefficients
+CALL correlation('pm25_atm_a', 'pm25_atm_b', 'purpleair_sensor_data', '');
+CALL correlation('pm25_atm_a', 'pm25_cf_1_a', 'purpleair_sensor_data', '');
+CALL correlation('pm25_atm_a', 'pm25_cf_1_b', 'purpleair_sensor_data', '');
+CALL correlation('pm25_atm_b', 'pm25_cf_1_a', 'purpleair_sensor_data', '');
+CALL correlation('pm25_atm_b', 'pm25_cf_1_b', 'purpleair_sensor_data', '');
+CALL correlation('pm25_cf_1_a', 'pm25_cf_1_b', 'purpleair_sensor_data', '');
+
+# Find the max PM2.5 value in the AirNow dataset
+SELECT MAX(pm25_nowcast_value) FROM airnow_sensor_data;
+
+# Limit PurpleAir dataset to data that falls below the max value appearance in the AirNow dataset
+CREATE OR REPLACE VIEW minimal_purpleair AS
+SELECT * FROM purpleair_sensor_data WHERE pm25_atm_a <= 202.1 AND pm25_atm_b <= 202.1 AND
+pm25_cf_1_a <= 202.1 AND pm25_cf_1_b <= 202.1;
+
+CALL correlation('pm25_atm_a', 'pm25_atm_b', 'minimal_purpleair', '');
+CALL correlation('pm25_atm_a', 'pm25_cf_1_a', 'minimal_purpleair', '');
+CALL correlation('pm25_atm_a', 'pm25_cf_1_b', 'minimal_purpleair', '');
+CALL correlation('pm25_atm_b', 'pm25_cf_1_a', 'minimal_purpleair', '');
+CALL correlation('pm25_atm_b', 'pm25_cf_1_b', 'minimal_purpleair', '');
+CALL correlation('pm25_cf_1_a', 'pm25_cf_1_b', 'minimal_purpleair', '');
 
 SELECT *
 FROM purpleair_sensor_data
@@ -79,8 +102,6 @@ UNION ALL
 SELECT AVG(pm25_cf_1_a) FROM purpleair_sensor_data WHERE pm25_cf_1_a > 300
 UNION ALL
 SELECT AVG(pm25_cf_1_b) FROM purpleair_sensor_data WHERE pm25_cf_1_b > 300;
-
-SELECT * FROM purpleair_sensor_data limit 10;
 
 # Function to get the distance between PurpleAir and AirNow sensors in miles
 DROP FUNCTION IF EXISTS haversine_miles;
@@ -121,173 +142,40 @@ SUM(dist_miles <= 1.0) AS `Under 1.0`,
 SUM(dist_miles <= 5.0) AS `Under 5.0`
 FROM sensor_distances;
 
-# Create view of PurpleAir dataset without outliers in ANY of the data rows
-CREATE OR REPLACE VIEW purpleair_cleaned_wo_outliers AS
-WITH
-atm_a_bound AS (
-    SELECT q3 + 1.5 * (q3 - q1) AS upper_bound
-    FROM (
-        SELECT
-            MAX(CASE WHEN pr <= 0.25 THEN pm25_atm_a END) AS q1,
-            MIN(CASE WHEN pr >= 0.75 THEN pm25_atm_a END) AS q3
-        FROM (
-            SELECT
-                pm25_atm_a,
-                PERCENT_RANK() OVER (ORDER BY pm25_atm_a) AS pr
-            FROM purpleair_sensor_data
-            WHERE pm25_atm_a > 0
-        ) x
-    ) y
-),
-atm_b_bound AS (
-    SELECT q3 + 1.5 * (q3 - q1) AS upper_bound
-    FROM (
-        SELECT
-            MAX(CASE WHEN pr <= 0.25 THEN pm25_atm_b END) AS q1,
-            MIN(CASE WHEN pr >= 0.75 THEN pm25_atm_b END) AS q3
-        FROM (
-            SELECT
-                pm25_atm_b,
-                PERCENT_RANK() OVER (ORDER BY pm25_atm_b) AS pr
-            FROM purpleair_sensor_data
-            WHERE pm25_atm_b > 0
-        ) x
-    ) y
-),
-cf1_a_bound AS (
-    SELECT q3 + 1.5 * (q3 - q1) AS upper_bound
-    FROM (
-        SELECT
-            MAX(CASE WHEN pr <= 0.25 THEN pm25_cf_1_a END) AS q1,
-            MIN(CASE WHEN pr >= 0.75 THEN pm25_cf_1_a END) AS q3
-        FROM (
-            SELECT
-                pm25_cf_1_a,
-                PERCENT_RANK() OVER (ORDER BY pm25_cf_1_a) AS pr
-            FROM purpleair_sensor_data
-            WHERE pm25_cf_1_a > 0
-        ) x
-    ) y
-),
-cf1_b_bound AS (
-    SELECT q3 + 1.5 * (q3 - q1) AS upper_bound
-    FROM (
-        SELECT
-            MAX(CASE WHEN pr <= 0.25 THEN pm25_cf_1_b END) AS q1,
-            MIN(CASE WHEN pr >= 0.75 THEN pm25_cf_1_b END) AS q3
-        FROM (
-            SELECT
-                pm25_cf_1_b,
-                PERCENT_RANK() OVER (ORDER BY pm25_cf_1_b) AS pr
-            FROM purpleair_sensor_data
-            WHERE pm25_cf_1_b > 0
-        ) x
-    ) y
-)
-SELECT p.*
+# Match corresponding rows from each data set based on the closest sensor
+CREATE OR REPLACE VIEW data_matched AS
+SELECT p.*, s.site_id, s.dist_miles, a.pm25_nowcast_value, a.pm25_raw_concentration
 FROM purpleair_sensor_data p
-CROSS JOIN atm_a_bound a CROSS JOIN atm_b_bound b CROSS JOIN cf1_a_bound c CROSS JOIN cf1_b_bound d
-WHERE (p.pm25_atm_a = 0 OR p.pm25_atm_a <= a.upper_bound)
-AND (p.pm25_atm_b = 0 OR p.pm25_atm_b <= b.upper_bound)
-AND (p.pm25_cf_1_a = 0 OR p.pm25_cf_1_a <= c.upper_bound)
-AND (p.pm25_cf_1_b = 0 OR p.pm25_cf_1_b <= d.upper_bound);
+JOIN sensor_distances s
+ON p.sensor_index = s.sensor_index
+JOIN airnow_sensor_data a
+ON s.site_id = a.site_id AND p.datetime_timestamp = a.datetime_timestamp;
 
-SELECT * FROM purpleair_cleaned_wo_outliers;
-
-# Procedure for creating views for each PurpleAir data column without outliers
-DROP PROCEDURE IF EXISTS make_col_wo_outliers;
-DELIMITER //
-CREATE PROCEDURE make_col_wo_outliers(
-    IN col_name VARCHAR(64),
-    IN view_name VARCHAR(64)
-)
-BEGIN
-    SET @sql = CONCAT(
-        'CREATE OR REPLACE VIEW ', view_name, ' AS
-         WITH positive_ranked AS (
-             SELECT ', col_name, ', PERCENT_RANK() OVER (ORDER BY ', col_name, ') AS pr
-             FROM purpleair_sensor_data
-             WHERE ', col_name, ' > 0
-         ),
-         quartiles AS (
-             SELECT
-                 MAX(CASE WHEN pr <= 0.25 THEN ', col_name, ' END) AS q1,
-                 MIN(CASE WHEN pr >= 0.75 THEN ', col_name, ' END) AS q3
-             FROM positive_ranked
-         ),
-         bounds AS (
-             SELECT
-                 q1,
-                 q3,
-                 (q3 - q1) AS iqr,
-                 (q3 + 1.5 * (q3 - q1)) AS upper_bound
-             FROM quartiles
-         )
-         SELECT p.*
-         FROM purpleair_sensor_data p
-         CROSS JOIN bounds b
-         WHERE p.', col_name, ' = 0
-            OR p.', col_name, ' <= b.upper_bound'
-    );
-
-    PREPARE stmt FROM @sql;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-END //
-DELIMITER ;
-
-CALL make_col_wo_outliers('pm25_atm_a', 'purpleair_atm_a_no_outliers');
-CALL make_col_wo_outliers('pm25_atm_b', 'purpleair_atm_b_no_outliers');
-CALL make_col_wo_outliers('pm25_cf_1_a', 'purpleair_cf1_a_no_outliers');
-CALL make_col_wo_outliers('pm25_cf_1_b', 'purpleair_cf1_b_no_outliers');
-
-SELECT * FROM purpleair_atm_a_no_outliers;
-SELECT * FROM purpleair_atm_b_no_outliers;
-SELECT * FROM purpleair_cf1_a_no_outliers;
-SELECT * FROM purpleair_cf1_b_no_outliers;
-
-SELECT * FROM sensor_distances;
-
-SELECT * FROM airnow_sensor_data JOIN airnow_sites ON
-airnow_sensor_data.fullaqscode = airnow_sites.fullaqscode;
-
-SELECT * FROM airnow_sensor_data;
-
-SELECT * FROM purpleair_sensor_data;
-
-SELECT * FROM purpleair_cleaned_wo_outliers limit 50;
-
-SELECT * FROM sensor_distances;
-
-SELECT site_id, COUNT(site_id)
-FROM sensor_distances
-GROUP BY site_id;
-
-CREATE OR REPLACE VIEW data_matched_w_outliers AS
-SELECT p.sensor_index AS sensor_index, p.datetime_timestamp AS datetime_timestamp, 
-humidity, temperature, pm25_atm_a, pm25_atm_b, pm25_cf_1_a, pm25_cf_1_b, 
-n.site_id AS site_id, pm25_nowcast_value, pm25_raw_concentration
+CREATE OR REPLACE VIEW minimal_data_matched AS
+SELECT p.*, s.site_id, s.dist_miles, a.pm25_nowcast_value, a.pm25_raw_concentration
 FROM purpleair_sensor_data p
-JOIN sensor_distances n
-ON p.sensor_index = n.sensor_index
+JOIN sensor_distances s
+ON p.sensor_index = s.sensor_index
 JOIN airnow_sensor_data a
-ON n.site_id = a.site_id AND p.datetime_timestamp = a.datetime_timestamp;
+ON s.site_id = a.site_id AND p.datetime_timestamp = a.datetime_timestamp
+WHERE pm25_atm_a <= 202.1 AND pm25_atm_b <= 202.1 AND
+pm25_cf_1_a <= 202.1 AND pm25_cf_1_b <= 202.1;
 
-CREATE OR REPLACE VIEW data_matched_wo_outliers AS
-SELECT p.sensor_index AS sensor_index, p.datetime_timestamp AS datetime_timestamp, 
-humidity, temperature, pm25_atm_a, pm25_atm_b, pm25_cf_1_a, pm25_cf_1_b, 
-n.site_id AS site_id, pm25_nowcast_value, pm25_raw_concentration
-FROM purpleair_cleaned_wo_outliers p
-JOIN sensor_distances n
-ON p.sensor_index = n.sensor_index
-JOIN airnow_sensor_data a
-ON n.site_id = a.site_id AND p.datetime_timestamp = a.datetime_timestamp
-WHERE p.pm25_cf_1_a <= 100;
+# Get the % of rows in PurpleAir dataset that are greater than the max appearence in AirNow dataset
+WITH max_airnow AS (
+	SELECT MAX(pm25_raw_concentration) AS the_max FROM airnow_sensor_data
+)
+SELECT ROUND(SUM(CASE WHEN pm25_cf_1_b > max_airnow.the_max 
+OR pm25_atm_a > max_airnow.the_max 
+OR pm25_atm_b > max_airnow.the_max 
+OR pm25_cf_1_b > max_airnow.the_max 
+THEN 1 ELSE 0 END)/COUNT(*)*100.0,2) AS '%'
+FROM purpleair_sensor_data CROSS JOIN max_airnow;
 
 DROP PROCEDURE IF EXISTS get_corr;
 DELIMITER //
 CREATE PROCEDURE get_corr(IN x_col VARCHAR(64), IN y_col VARCHAR(64),
-IN table_name VARCHAR(64))
+IN table_name VARCHAR(64), IN where_clause VARCHAR(999))
 BEGIN
     SET @sql = CONCAT(
         'SELECT ',
@@ -304,7 +192,7 @@ BEGIN
         ') AS correlation ',
         'FROM ', table_name, ' ',
         'WHERE ', x_col, ' IS NOT NULL ',
-        'AND ', y_col, ' IS NOT NULL'
+        'AND ', y_col, ' IS NOT NULL ' , where_clause
     );
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
@@ -312,17 +200,28 @@ BEGIN
 END //
 DELIMITER ;
 
-# Get correlation coefficicient for outlier included data
-CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched_w_outliers');
+# Get correlation coefficicient for all data
+CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'data_matched', '');
+CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'data_matched', '');
+CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched', '');
+CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched', '');
 
-# Get correlation coefficicient for outlier excluded data
-CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched_wo_outliers');
+SELECT * FROM minimal_data_matched;
+
+CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'minimal_data_matched', '');
+CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'minimal_data_matched', '');
+CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'minimal_data_matched', '');
+CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'minimal_data_matched', '');
+
+CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'minimal_data_matched', 'AND dist_miles < 0.5');
+CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'minimal_data_matched', 'AND dist_miles < 0.5');
+CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'minimal_data_matched', 'AND dist_miles < 0.5');
+CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'minimal_data_matched', 'AND dist_miles < 0.5');
+
+CALL get_corr('pm25_atm_a', 'pm25_nowcast_value', 'data_matched', ' AND pm25_atm_a < 202.1');
+CALL get_corr('pm25_atm_b', 'pm25_nowcast_value', 'data_matched', 'AND pm25_atm_b < 202.1');
+CALL get_corr('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched', 'AND pm25_cf_1_a < 202.1');
+CALL get_corr('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched', 'AND pm25_cf_1_b < 202.1');
 
 DROP PROCEDURE IF EXISTS get_mae;
 DELIMITER //
@@ -347,12 +246,19 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL get_mae('pm25_atm_a', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_mae('pm25_atm_b', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_mae('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched_w_outliers');
-CALL get_mae('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched_w_outliers');
+CALL get_mae('pm25_atm_a', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_atm_b', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched');
 
-CALL get_mae('pm25_atm_a', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_mae('pm25_atm_b', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_mae('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched_wo_outliers');
-CALL get_mae('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched_wo_outliers');
+CALL get_mae('pm25_atm_a', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_atm_b', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_cf_1_a', 'pm25_nowcast_value', 'data_matched');
+CALL get_mae('pm25_cf_1_b', 'pm25_nowcast_value', 'data_matched');
+
+SELECT MAX(pm25_atm_a) FROM temp_purpleair_cleaned_wo_outliers;
+SELECT MAX(pm25_atm_b) FROM temp_purpleair_cleaned_wo_outliers;
+SELECT MAX(pm25_cf_1_a) FROM temp_purpleair_cleaned_wo_outliers;
+SELECT MAX(pm25_cf_1_b) FROM temp_purpleair_cleaned_wo_outliers;
+
+SELECT MAX(pm25_nowcast_value) FROM airnow_sensor_data
