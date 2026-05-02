7231c3ddd459c7caa4c283a378a5dc9fb4ff108a
shamb1es <juwright24@gmail.com>
2026-05-01 22:10:58 -0400

    Placed data in Power BI

--- sql/playground.sql
+++ sql/playground.sql
@@ -121,10 +121,18 @@ SELECT AVG(pm25_atm_a), datetime_timestamp FROM purpleair_sensor_data
 WHERE pm25_atm_a <= 202.1 AND pm25_atm_b <= 202.1 AND pm25_cf_1_a <= 202.1 AND pm25_cf_1_b <= 202.1
 group by datetime_timestamp 
 ORDER BY datetime_timestamp ASC LIMIT 10;
-
+select count(*) from purpleair_sensor_data;
+select count(*) from data_matched;
 SELECT * 
 FROM purpleair_sensor_data
 INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/purpleair_cleaned.csv'
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+select * from data_matched limit 5;
+SELECT * 
+FROM data_matched
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data_matched.csv'
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
